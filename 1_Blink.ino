843a35fd966bbefa8d871865353f9b9d951530c7
Adrian Baaru <gatotoadrian@gmail.com>
2024-05-30 21:04:29 +0300

    added the code

--- 1_Blink.ino
+++ 1_Blink.ino
@@ -1,27 +1,15 @@
-# LED Blink Project for NodeMCU-32S
 
-This project demonstrates how to blink the built-in LED on the NodeMCU-32S board using Arduino code. The LED is connected to pin 2 on the NodeMCU-32S, and it will blink on and off with a delay of one second.
+// the setup function runs once when you press reset or power the board
+void setup() {
+  // initialize digital pin LED_BUILTIN as an output.
+  // the LED_BUILTIN is also pin no 2 on the nodemcu-32s
+  pinMode(LED_BUILTIN, OUTPUT);
+}
 
-## Requirements
-
-- NodeMCU-32S board
-- Arduino IDE
-
-## Setup
-
-1. **Install the Arduino IDE**
-
-   If you haven't already, download and install the Arduino IDE from the [official website](https://www.arduino.cc/en/Main/Software).
-
-2. **Install ESP32 Board Support**
-
-   Open the Arduino IDE and follow these steps to install the ESP32 board support:
-
-   - Go to `File` > `Preferences`.
-   - In the "Additional Board Manager URLs" field, add the following URL: `https://dl.espressif.com/dl/package_esp32_index.json`
-   - Go to `Tools` > `Board` > `Boards Manager`.
-   - Search for `ESP32` and install the latest version of the `esp32` package by Espressif Systems.
-
-3. **Select the NodeMCU-32S Board**
-
-   - Go to `Tools` > `Board` and select `NodeMCU-32S`.
+// the loop function runs over and over again forever
+void loop() {
+  digitalWrite(LED_BUILTIN, HIGH);  // turn the LED on (HIGH is the voltage level)
+  delay(1000);                      // wait for a second
+  digitalWrite(LED_BUILTIN, LOW);   // turn the LED off by making the voltage LOW
+  delay(1000);                      // wait for a second
+}
